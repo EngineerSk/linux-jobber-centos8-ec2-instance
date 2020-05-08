b67f2fc75dc2dd915ca3289d8351f0b71ef6aad3
EngineerSk <eskayoriade@gmail.com>
2020-05-08 07:39:03 +0100

    updated myscrumy/urls.py

--- myscrumy/myscrumy/urls.py
+++ myscrumy/myscrumy/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-	path('eskayoriadescrumy/', include('eskayoriadescrumy.urls.py')),
+	path('eskayoriadescrumy/', include('eskayoriadescrumy/urls.py')),
     path('admin/', admin.site.urls),
 ]
