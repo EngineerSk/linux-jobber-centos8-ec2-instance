3d85ab05a2733bf32fae473b2b3f9fe2e5442e3e
EngineerSk <eskayoriade@gmail.com>
2020-05-13 02:31:52 +0000

    configured eskayoriadescrumy app for redistributuion

--- myscrumy/myscrumy/settings.py
+++ myscrumy/myscrumy/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['18.191.220.71', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'eskayoriadescrumy.apps',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
