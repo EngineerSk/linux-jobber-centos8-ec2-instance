00023c93a8ac1fe3f38e6dc4db91ea9d035ef127
EngineerSk <eskayoriade@gmail.com>
2020-05-18 15:40:36 +0000

    Creating Django views that accept arguments (lab 11)

--- myscrumy/eskayoriadescrumy/views.py
+++ myscrumy/eskayoriadescrumy/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import ScrumyGoals
 
 # Create your views here.
 def index(request):
     return HttpResponse("This is a Scrum Application")
+
+def move_goal(request, goal_id):
+    myId = goal_id
+    goal =  ScrumyGoals.objects.get(goal_id=myId)
+    return HttpResponse(goal)
