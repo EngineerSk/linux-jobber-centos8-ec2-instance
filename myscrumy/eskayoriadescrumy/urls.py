2b2059b8725c18df4557567335b7318ced5bc0f3
EngineerSk <eskayoriade@gmail.com>
2020-05-20 07:12:27 +0000

    Labs for Creating Views that access the Database (lab 12)

--- myscrumy/eskayoriadescrumy/urls.py
+++ myscrumy/eskayoriadescrumy/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('movegoal/<int:goal_id>', views.move_goal),
-    path('addgoal', views.add_goal, name='add_goal'),
-    path('home', views.home, name='home')
+    path('addgoal', views.add_goal),
+    path('home', views.home)
 ]
