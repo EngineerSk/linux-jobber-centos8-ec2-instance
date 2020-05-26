7b94e17bf85ab6baa6360e073547874b2612340e
EngineerSk <eskayoriade@gmail.com>
2020-05-26 13:08:54 +0000

    Labs for Replacing Hardcoded URLs with Dynamic Values and URLs Namespacing

--- myscrumy/eskayoriadescrumy/urls.py
+++ myscrumy/eskayoriadescrumy/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
 from . import views
 
+app_name = 'eskayoriadescrumy'
+
 urlpatterns = [
-    path('', views.index),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', views.index, name='index'),
     path('movegoal/<int:goal_id>', views.move_goal),
     path('addgoal', views.add_goal),
     path('home', views.home)
