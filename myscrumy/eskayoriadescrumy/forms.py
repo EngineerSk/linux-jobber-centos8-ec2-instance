cbff590e33907171988ed99ac8dd0417a200e873
EngineerSk <eskayoriade@gmail.com>
2020-05-29 13:25:28 +0100

    lab 18 tested OK (Labs for Submitting and Displaying Form Results)

---
+++ myscrumy/eskayoriadescrumy/forms.py
@@ -0,0 +1,13 @@
+from django.forms import ModelForm
+from .models import ScrumyGoals, User
+
+class SignupForm(ModelForm):
+	class Meta:
+		model = User
+		fields = ['first_name', 'last_name', 'email', 'username', 'password']
+
+
+class CreateGoalForm(ModelForm):
+	class Meta:
+		model = ScrumyGoals
+		fields = ['goal_name', 'user']
